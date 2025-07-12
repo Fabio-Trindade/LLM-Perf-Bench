d147039180d049598688e52728f7915bf5da22f8
Fabio-Trindade <fabiotrindadefer@gmail.com>
2025-07-12 17:23:19 -0300

    fix: update dummy requester according previous changes

--- src/components/requesters/dummy_requester.py
+++ src/components/requesters/dummy_requester.py
@@ -13,6 +13,8 @@ class DummyRequester(RequesterI):
     async def async_request(self, queue: QueueI, buffer: PerformanceMetricsBuffer, server: ServerI):
         dummy_config = self.config
         load_config = self.config
+        assert(self.config.prompts_per_request == 1), "We do not allow more than 1 prompt per request. We have to to assert all prompts are the same max_out_len first."
+
         
         async def infer():
             await asyncio.sleep(dummy_config.infer_sleep_time)
@@ -20,21 +22,24 @@ class DummyRequester(RequesterI):
         prompts_per_request = load_config.prompts_per_request
         req_id = self.get_request_id()
         prompt_idx_list : list[int] = []
-
+        first_prompt = None
         for i in range(prompts_per_request):
             prompt, prompt_idx = await queue.get_prompt_and_idx_async()
-            buffer.initialize_metrics((req_id, i), prompt_idx, req_id, True)
+            buffer.initialize_metrics(prompt, (req_id, i), req_id, True)
             prompt_idx_list.append(prompt_idx)
+            if first_prompt is None:
+                first_prompt = prompt
+
     
         predicted_tokens = 0
-        total_target_pred_tokens = load_config.max_out_tokens * prompts_per_request 
+        total_target_pred_tokens = first_prompt.max_out_tokens * prompts_per_request 
 
         while predicted_tokens < total_target_pred_tokens:
              for i,idx in enumerate(prompt_idx_list):
                 await infer()
                 last_time = time.time()
                 key = (req_id, i)
-                buffer.add_out_token_time(key, last_time)
+                buffer.add_decode_data(key, last_time,f"token_{predicted_tokens}")
                 predicted_tokens += 1
                    
         
