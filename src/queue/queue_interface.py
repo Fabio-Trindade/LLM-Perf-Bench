54a2e846a43b07693cb0a08a0b9b57f2a2f7c614
Fabio-Trindade <fabiotrindadefer@gmail.com>
2025-07-12 17:09:33 -0300

    refactor: update return type

--- src/queue/queue_interface.py
+++ src/queue/queue_interface.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
 
+from src.data_structures.prompt import Prompt
+
 class QueueI:
 
     async def add_prompt_and_idx_async(self, prompt: str, idx: int) -> None:
         raise RuntimeError("Not implemented.")
     
-    async def get_prompt_and_idx_async(self) -> tuple[str, int]:
+    async def get_prompt_and_idx_async(self) -> tuple[Prompt, int]:
         raise RuntimeError("Not implemented.")    
     
     def empty(self) -> bool:
