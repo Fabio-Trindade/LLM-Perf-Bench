42f1b4b675a613dc2654120fca761f00dd5e1c8b
Fabio-Trindade <fabiotrindadefer@gmail.com>
2025-07-29 08:42:08 -0300

    add e2e goal

--- examples/load_exp/gen_load_examples_report.sh
+++ examples/load_exp/gen_load_examples_report.sh
@@ -2,4 +2,5 @@ python gen_report.py \
     --csv_path results/load_data.csv \
     --experiments "opt_125m_load_exp" \
     --type load \
-    --output_path results/reports/load/
+    --output_path results/reports/load/ \
+    --e2e_goal 1
