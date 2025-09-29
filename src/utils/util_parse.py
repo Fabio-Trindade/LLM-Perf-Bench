533ff921c8c758f4f248e0fe411f23bc5429031b
Fabio-Trindade <fabiotrindadefer@gmail.com>
2025-09-29 13:10:09 -0300

    refactor: change add_arg(...)

--- src/utils/util_parse.py
+++ src/utils/util_parse.py
@@ -1,11 +1,14 @@
 import argparse
 
 def add_arg(parser: argparse.ArgumentParser, name, fixed_values, **kwargs):
-    if name not in fixed_values:
-        parser.add_argument(f"--{name}", **kwargs)
-    else:
-        kwargs["default"] = fixed_values[name]
-        parser.add_argument(f"--{name}", **kwargs)
+    arg_name = f"--{name}"
+    if arg_name not in parser._option_string_actions:
+        if name not in fixed_values:
+            parser.add_argument(f"--{name}", **kwargs)
+        else:
+            kwargs["default"] = fixed_values[name]
+            parser.add_argument(f"--{name}", **kwargs)
+        
 
 def get_fixed_values(fixed_values):
     return  fixed_values or {}
