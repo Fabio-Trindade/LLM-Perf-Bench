4702befcc8058c0448248cc0ba8bd495d3b9c015
Fabio-Trindade <fabiotrindadefer@gmail.com>
2025-09-29 13:24:13 -0300

    refactor: add vLLM as a tokenizer component

--- src/catalogs/component_catalog.py
+++ src/catalogs/component_catalog.py
@@ -1,7 +1,7 @@
 class ComponentCatalog:
     _requester_instances = ["dummy","vllm", "openai"]
     _config_instances = ["dummy", "openai"]
-    _tokenizer_instances = ["HF", "openai", "whitespace"]
+    _tokenizer_instances = ["HF", "openai", "whitespace","vLLM"]
     _dataset_gen_instances = ["synthetic", "replay"]
     
     _types = ["parser", "workload"]
