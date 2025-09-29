8fd62af4c9a3a6c6a524f809ea30d902c2c03634
Fabio-Trindade <fabiotrindadefer@gmail.com>
2025-09-29 13:25:26 -0300

    feature: auto search for perf experiments

---
+++ find_best_num_parallel_batches_vllm.py
@@ -0,0 +1,72 @@
+import argparse
+import logging
+import os
+import signal
+from types import SimpleNamespace
+
+from src.prompt_generator.prompt_generator import PromptGeneratorBase
+from src.components.tokenizers.vllm_tokenizer import vLLMTokenizer
+from vllm import LLM, SamplingParams, LLM
+from src.utils.vllm_utils import kill_vllm_server_process, start_and_wait_vllm_server
+from src.utils.util_parse import add_arg
+
+def parse_best_batch_args(argparser: argparse.ArgumentParser, fixed_args):
+    add_arg(argparser, "model", fixed_args, type=str, default="")
+    add_arg(argparser, "prompt_size", fixed_args, type=int)
+    add_arg(argparser, "max_out_tokens", fixed_args, type=int, default=64)
+    add_arg(argparser, "initial_batch_size", fixed_args, type=int, default=1)
+    add_arg(argparser, "dtype", fixed_args, type=str, default="float16")
+    add_arg(argparser, "gpu_memory_utilization", fixed_args, type=float, default=0.9)
+    add_arg(argparser, "seed", fixed_args, type=int, default = 1234)
+    add_arg(argparser, "port", fixed_args, type=str, default = "8000")
+
+    
+def infer(port, model_name, prompt_size, max_out_tokens, num_seqs, dtype,gpu_memory_utilization, seed):
+    server_process = start_and_wait_vllm_server(model_name,port,prompt_size + max_out_tokens, num_seqs,dtype, gpu_memory_utilization,seed, "auto")
+    kill_vllm_server_process(server_process)
+
+def find_best_num_parallel_batches_vllm(model_name, prompt_size, max_out_tokens, initial_batch_size, dtype,
+                                        gpu_memory_utilization, seed, port):
+    if model_name is None or model_name == "":
+        raise ValueError("model_name is None or empty string")
+
+    # config = SimpleNamespace(model=model_name)
+    # tokenizer = vLLMTokenizer(config)
+    # prompt_generator = PromptGeneratorBase()
+    # prompt = prompt_generator.gen_prompt(tokenizer, prompt_size, max_out_tokens)
+
+    last_valid_batch_size = 0
+    last_worst_batch_size = float("inf")
+    cur_batch_size = initial_batch_size
+    num_out_of_memory = 0
+    while cur_batch_size != last_valid_batch_size:
+        # prompts = [prompt.prompt] * cur_batch_size
+        try:
+            infer(port,model_name, prompt_size, max_out_tokens, cur_batch_size, dtype,gpu_memory_utilization,seed)
+            last_valid_batch_size = cur_batch_size
+            if last_worst_batch_size == float("inf"):
+                cur_batch_size *= 2
+            else:
+                cur_batch_size = (last_valid_batch_size + last_worst_batch_size) // 2
+        except Exception as e:
+            last_worst_batch_size = cur_batch_size
+            cur_batch_size = (last_valid_batch_size + cur_batch_size) // 2
+            num_out_of_memory += 1
+    return last_valid_batch_size
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    parse_best_batch_args(argparser, {})
+    args = argparser.parse_args()
+    
+    max_num_seqs = find_best_num_parallel_batches_vllm(
+        args.model, args.prompt_size, args.max_out_tokens, args.initial_batch_size,
+        args.dtype, args.gpu_memory_utilization, args.seed, args.port
+    )
+
+    print(f"\n\nBest batch size (vLLM): {max_num_seqs}\n\n")
+
+
+
+    
+
---
+++ find_best_num_requesters.py
@@ -0,0 +1,61 @@
+import argparse
+import copy
+from run_load_exp import add_load_exp_args, config_load_exp
+from src.utils.util_import import initialize_modules
+from src.utils.util_experiment import get_args_from_parser, run_workload
+from src.utils.util_parse import add_arg
+
+initialize_modules()
+    
+def parse_best_num_requester_args(parser, fixed_args):
+    add_arg(parser, "initial_num_requester", fixed_args, type=int, default=60,
+            help="Initial run time in seconds.")
+    add_arg(parser, "max_num_requesters", fixed_args, type=int, default=600,
+            help="Maximum run time in seconds.")
+    add_arg(parser, "increment_requester_value", fixed_args, type=int, default=60,
+            help="Increment time in seconds.")
+    add_arg(parser, "epsilon_requester", fixed_args, type=float, default=0.05,
+            help="Epsilon for throughput convergence.")
+
+
+def find_best_num_requesters(config):
+    tokenizer, dataset_gen, server, requester, prompt_generator = config_load_exp(config)
+    epsilon = config.epsilon_requester
+    max_num_requester = config.max_num_requesters
+    increment_value = config.increment_requester_value
+    cur_num_requester = config.initial_num_requester
+    last_thp = None
+
+    while cur_num_requester <= max_num_requester:
+        config.num_requester_threads = cur_num_requester
+        perf_results = run_workload(config, tokenizer, prompt_generator, server, requester, dataset_gen, last_thp == None, cur_num_requester == max_num_requester)[0]
+        
+        if last_thp is None:
+            last_thp = perf_results.calc_total_throughput()
+        else:
+            cur_thp = perf_results.calc_total_throughput()
+            if 1 - epsilon <= cur_thp/last_thp <= 1 + epsilon:
+                print(f"Last thp: {last_thp} - Current thp: {cur_thp} - var = {cur_thp/last_thp}")
+                break
+            else:
+                print(f"Last thp: {last_thp} - Current thp: {cur_thp} - var = {cur_thp/last_thp}")
+                last_thp = cur_thp
+        cur_num_requester += increment_value
+
+    server.shutdown()
+    return cur_num_requester - increment_value, last_thp
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    fixed_args = {
+        "num_prompts": 1,
+        "num_requesters": [None]
+    }
+
+    args = get_args_from_parser(parser, fixed_args, add_load_exp_args, parse_best_num_requester_args)
+
+    args = parser.parse_args()
+    best_num_req, thp = find_best_num_requesters(config = args)
+    print(f"Best num requester: {best_num_req} with throughput {thp}") 
+
+
+
